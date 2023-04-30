1fe27baa597efd6b61f7585e335c5bb1bfa444b3
sandro520099 <sandro520099@hotmail.com>
2023-04-30 14:49:29 -0300

    Update stop.js

--- stop.js
+++ stop.js
@@ -1,1 +1,8 @@
-
+Copy code
+// fecha todas as abas do navegador
+function closeAllTabs() {
+  // obtém uma referência à janela principal do navegador
+  var windowList = window.open("", "_self");
+  // fecha a janela principal do navegador
+  windowList.close();
+}
